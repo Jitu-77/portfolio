60c18d7e75bb476aa8e8cfc48a7121da9a6a8a25
Jitu-77 <jyotirmoypal077@gmail.com>
2026-03-10 00:44:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Portfolio Setup Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
